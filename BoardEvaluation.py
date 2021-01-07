fd8a43d814b7959664c3ee597f3bbe0bfd7c183b
Empreon <77062493+Empreon@users.noreply.github.com>
2021-01-07 17:36:53 +0300

    Update BoardEvaluation.py

--- BoardEvaluation.py
+++ BoardEvaluation.py
@@ -1,11 +1,11 @@
 import chess
 from PieceData import pieceDataBase
 
+# https://andreasstckl.medium.com/writing-a-chess-program-in-one-day-30daff4610ec
 class boardEvaluate:
     def evaluation(self, fenNotation):
         board = chess.Board(fenNotation)
 
-        # https://andreasstckl.medium.com/writing-a-chess-program-in-one-day-30daff4610ec
         if board.is_checkmate():
             if board.turn:
                 return -9999
@@ -27,13 +27,31 @@ class boardEvaluate:
         wq = len(board.pieces(chess.QUEEN, chess.WHITE))
         bq = len(board.pieces(chess.QUEEN, chess.BLACK))
 
-        material = pieceDataBase.pawnValue * (wp - bp) + \
-                   pieceDataBase.kingValue * (wn - bn) + \
-                   pieceDataBase.bishopValue * (wb - bb) + \
-                   pieceDataBase.rookValue * (wr - br) + \
-                   pieceDataBase.queenValue * (wq - bq)
+        material = pieceDataBase.pawnValue * (wp - bp) + pieceDataBase.knightValue * (
+                    wn - bn) + pieceDataBase.bishopValue * (wb - bb) + pieceDataBase.rookValue * (
+                               wr - br) + pieceDataBase.queenValue * (wq - bq)
 
+        pawnsq = sum([pieceDataBase.P[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+        pawnsq = pawnsq + sum([-pieceDataBase.P[chess.square_mirror(i)]
+                               for i in board.pieces(chess.PAWN, chess.BLACK)])
+        knightsq = sum([pieceDataBase.N[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+        knightsq = knightsq + sum([-pieceDataBase.N[chess.square_mirror(i)]
+                                   for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+        bishopsq = sum([pieceDataBase.B[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+        bishopsq = bishopsq + sum([-pieceDataBase.B[chess.square_mirror(i)]
+                                   for i in board.pieces(chess.BISHOP, chess.BLACK)])
+        rooksq = sum([pieceDataBase.R[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+        rooksq = rooksq + sum([-pieceDataBase.R[chess.square_mirror(i)]
+                               for i in board.pieces(chess.ROOK, chess.BLACK)])
+        queensq = sum([pieceDataBase.Q[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+        queensq = queensq + sum([-pieceDataBase.Q[chess.square_mirror(i)]
+                                 for i in board.pieces(chess.QUEEN, chess.BLACK)])
+        kingsq = sum([pieceDataBase.K[i] for i in board.pieces(chess.KING, chess.WHITE)])
+        kingsq = kingsq + sum([-pieceDataBase.K[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KING, chess.BLACK)])
+
+        eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
         if board.turn:
-            return material
+            return eval
         else:
-            return -material
+            return -eval
