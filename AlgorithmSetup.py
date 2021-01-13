2439d70f12c2803bc674285b8079f26d61fbd323
Empreon <77062493+Empreon@users.noreply.github.com>
2021-01-13 15:55:52 +0300

    Update AlgorithmSetup.py

--- AlgorithmSetup.py
+++ AlgorithmSetup.py
@@ -3,15 +3,6 @@ import chess.polyglot
 
 from BoardEvaluation_1 import evaluation
 
-board = chess.Board("2k5/8/3K4/8/8/8/8/7R w - - 0 1")
-
-def minimaxMoveData(x):
-    moveData = []
-    for firstMove in x.legal_moves:
-        moveData.append(firstMove)
-
-    return moveData
-
 # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 def keywithmaxval(d):
     v = list(d.values())
@@ -23,56 +14,55 @@ def keywithminval(d):
     k = list(d.keys())
     return k[v.index(min(v))]
 
-def moveDeneme(move, list):
-    board.push(move)
-    if board.is_game_over():
-        list.append(evaluation(board))
-
-def minimaxDeneme():
-    a = []
-    b = minimaxMoveData(board)
-    c = []
-    for move0 in board.legal_moves:
-        moveDeneme(move0, c)
-        if not board.is_game_over():
-            for move1 in board.legal_moves:
-                moveDeneme(move1, c)
-                if not board.is_game_over():
-                    for move2 in board.legal_moves:
-                        moveDeneme(move2, c)
-                        if not board.is_game_over():
-                            for move3 in board.legal_moves:
-                                moveDeneme(move3, c)
-                                if not board.is_game_over():
-                                    for move4 in board.legal_moves:
-                                        board.push(move4)
-                                        c.append(evaluation(board))
-                                        board.pop()
-                                board.pop()
-                        board.pop()
-                board.pop()
-        board.pop()
-
-        a.append(min(c))
-        c.clear()
-
-    baDict = dict(zip(b, a))
-
-    return baDict
-
-def bookMove():
+def bookMove(board):
     book = []
     move = chess.polyglot.MemoryMappedReader("kasparov.bin").weighted_choice(board).move
     book.append(move)
 
     return book
 
+def moveDeneme(board, move, list):
+    board.push(move)
+    if board.is_game_over():
+        list.append(evaluation(board))
 
-def moveSelect():
+def minimaxDeneme(board):
+    minValues = []
+    moves = board.legal_moves
+    allValues = []
+    for move0 in board.legal_moves:
+        moveDeneme(board, move0, allValues)
+        if not board.is_game_over():
+            for move1 in board.legal_moves:
+                moveDeneme(board, move1, allValues)
+                if not board.is_game_over():
+                    for move2 in board.legal_moves:
+                        moveDeneme(board, move2, allValues)
+                        if not board.is_game_over():
+                            for move3 in board.legal_moves:
+                                moveDeneme(board, move3, allValues)
+                                if not board.is_game_over():
+                                    for move4 in board.legal_moves:
+                                        board.push(move4)
+                                        allValues.append(evaluation(board))
+                                        board.pop()
+                                board.pop()
+                        board.pop()
+                board.pop()
+        board.pop()
+        minValues.append(min(allValues))
+        allValues.clear()
+
+    baDict = dict(zip(moves, minValues))
+    return baDict
+
+def moveSelect(board):
     try:
-        bookMoveList = bookMove()
+        bookMoveList = bookMove(board)
         return bookMoveList[0]
     except:
         print("move from algorithm")
-        maxMove = keywithmaxval(minimaxDeneme())
+        maxMove = keywithmaxval(minimaxDeneme(board))
         return maxMove
+
+chessboard = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
