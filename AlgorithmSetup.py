05c960327733043a56aaa4aeba9a6800737e0a86
Empreon <77062493+Empreon@users.noreply.github.com>
2021-02-14 12:20:42 +0300

    Update AlgorithmSetup.py

--- AlgorithmSetup.py
+++ AlgorithmSetup.py
@@ -1,73 +1,99 @@
 import chess
 import chess.polyglot
 
-from BoardEvaluation_1 import evaluation
+from BoardEvaluation import evaluation
 
-# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
-def keywithmaxval(d):
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(max(v))]
-
-def keywithminval(d):
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(min(v))]
-
-def bookMoves(board):
-    book = []
-    bookmove = chess.polyglot.MemoryMappedReader("kasparov.bin").weighted_choice(board).move
-    book.append(bookmove)
-    return book
-
-# https://www.youtube.com/watch?v=l-hh51ncgDI
-def minimaxDeneme(board, depth, alpha, beta, turn):
-    if depth == 0 or board.is_game_over():
-        return evaluation(board)
-    if turn:
-        maxValue = -9999
-        for whiteMove in board.legal_moves:
-            board.push(whiteMove)
-            value = minimaxDeneme(board, depth - 1, alpha, beta, False)
+# https://github.com/astoeckl/mediumchess
+# https://github.com/SebLague/Chess-AI
+def moveDataOrder(board):
+    moveList = []
+    moveValue = []
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)
+            moveList.append(move)
+            if board.is_check():
+                x = 250
+            else:
+                x = 0
+            if board.turn:
+                moveValue.append(evaluation(board) + (150 + x))
+            else:
+                moveValue.append(evaluation(board) - (150 + x))
             board.pop()
-            maxValue = max(maxValue, value)
-            alpha = max(alpha, value)
-            if beta <= alpha:
-                break
-        return maxValue
+        else:
+            board.push(move)
+            moveList.append(move)
+            if board.is_check():
+                x = 250
+            else:
+                x = 0
+            if board.turn:
+                moveValue.append(evaluation(board) + x)
+            else:
+                moveValue.append(evaluation(board) - x)
+            board.pop()
+
+# https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    moveOrder = []
+    moves = dict(zip(moveList, moveValue))
+    if board.turn:
+        moves = dict(sorted(moves.items(), key=lambda item: item[1]))
     else:
-        minValue = 9999
-        for blackMove in board.legal_moves:
-            board.push(blackMove)
-            value = minimaxDeneme(board, depth - 1, alpha, beta, True)
-            board.pop()
-            minValue = min(minValue, value)
-            beta = min(beta, value)
-            if beta <= alpha:
-                break
-        return minValue
+        moves = dict(sorted(moves.items(), key=lambda item: item[1], reverse=True))
+    for key in moves.keys():
+        moveOrder.append(key)
 
-# https://andreasstckl.medium.com/writing-a-chess-program-in-one-day-30daff4610ec
+    return moveOrder
+
+def moveSearch(depth, alpha, beta, board):
+    bestScore = -9999
+    if depth == 0:
+        return captureSearch(alpha, beta, board)
+    for move in board.legal_moves:
+        board.push(move)
+        score = -moveSearch(depth - 1, -alpha, -beta, board)
+        board.pop()
+        if score >= beta:
+            return score
+        if score > bestScore:
+            bestScore = score
+        if score > alpha:
+            alpha = score
+    return bestScore
+
+def captureSearch(alpha, beta, board):
+    preScore = evaluation(board)
+    if preScore >= beta:
+        return beta
+    if alpha < preScore:
+        alpha = preScore
+
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)
+            score = -captureSearch(-alpha, -beta, board)
+            board.pop()
+
+            if score >= beta:
+                return beta
+            if score > alpha:
+                alpha = score
+    return alpha
+
 def moveSelect(board, depth):
-    try:
-        bookMoveList = bookMoves(board)
-        print("move from book")
-        return bookMoveList[0]
-    except:
-        goodMove = chess.Move.null()
-        bestValue = -9999
-        alpha = -10000
-        beta = 10000
-        print("move from code")
-        for codemove in board.legal_moves:
-            board.push(codemove)
-            boardValue = minimaxDeneme(board, depth - 1, alpha, beta, board.turn)
-            if boardValue > bestValue:
-                bestValue = boardValue
-                goodMove = codemove
-            if boardValue > alpha:
-                alpha = boardValue
-            board.pop()
-        return goodMove
-
-chessboard = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    goodMove = chess.Move.null()
+    bestValue = -99999
+    alpha = -100000
+    beta = 100000
+    legalmoveList = moveDataOrder(board)
+    for move in legalmoveList:
+        board.push(move)
+        boardValue = -moveSearch(depth - 1, -alpha, -beta, board)
+        if boardValue > bestValue:
+            bestValue = boardValue
+            goodMove = move
+        if boardValue > alpha:
+            alpha = boardValue
+        board.pop()
+    return goodMove
